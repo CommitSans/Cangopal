2266b2a99b1481361684892c9c4c1ce121dd68c0
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-08 15:05:35 +0100

    Also watch localization files to update i18n

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var exec = require('child_process').exec;
 
 const dist_folder = 'docs';
 const source_folder = 'www';
+const locales_folder = 'locales';
  
 gulp.task('i18n', function (cb) {
   exec('static-i18n -l es -i en -i es -o '+ dist_folder +' '+ source_folder, function (err, stdout, stderr) {
@@ -30,6 +31,7 @@ gulp.task('build', gulp.series('i18n', 'styles', 'scripts'));
 gulp.task('watch', function () {
   livereload.listen();
   gulp.watch(source_folder + '/**/*.html', gulp.series('i18n'));
+  gulp.watch(locales_folder + '/**/*', gulp.series('i18n'));
   gulp.watch(source_folder + '/**/*.css', gulp.series('styles'));
   gulp.watch(source_folder + '/**/*.js', gulp.series('scripts'));
 });
